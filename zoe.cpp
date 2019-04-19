84003781b8e41d7366022fe46ff1b935e2334102
zhouliangyu <zhouliang.ben.yu@gmail.com>
2019-04-19 14:54:18 -0700

    new

--- zoe.cpp
+++ zoe.cpp
@@ -10,8 +10,34 @@ int main(int argc, char *argv[])
 	int y, x;
 	if (argc != 2)
 	{
-		
+		printf("Usage: %s <a c file name>\n", argv[0]);
+		exit(1);
 	}
-	
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		perror("Cannot open input file");
+		exit(1);
+	}
+	initscr();
+	getmaxyx(stdscr, row, col);
+	while ((ch = fgetc(fp)) != EOF)
+	{
+		getyx(stdscr, y, x);
+		if (y == (row-1))
+		{
+			attron(A_REVERSE);
+			printw("< - Press Any Key - >");
+			attroff(A_REVERSE);
+			getch();
+			clear();
+			move(0, 0);
+		}
+		printw("%c", ch);
+	}
+	printw("<END>");
+	getch();
+	endwin();
+	fclose(fp);
 	return 0;
 }
