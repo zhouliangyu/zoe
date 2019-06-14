b8f181b05d2404afc938d52c538eeb0bce550a1a
zhouliangyu <zhouliang.ben.yu@gmail.com>
2019-06-14 11:35:57 -0700

    not sure

--- zoe.cpp
+++ zoe.cpp
@@ -1,43 +1,23 @@
 #include <ncurses.h>
-#include <stdlib.h>
-#include <string.h>
+
+WINDOW *create_newwin(int height, int width, int startx, int starty);
+void destroy_win(WINDOW *local_win);
 
 int main(int argc, char *argv[])
 {
-	int ch, prev, row, col;
-	prev = EOF;
-	FILE *fp;
-	int y, x;
-	if (argc != 2)
-	{
-		printf("Usage: %s <a c file name>\n", argv[0]);
-		exit(1);
-	}
-	fp = fopen(argv[1], "r");
-	if (fp == NULL)
-	{
-		perror("Cannot open input file");
-		exit(1);
-	}
-	initscr();
-	getmaxyx(stdscr, row, col);
-	while ((ch = fgetc(fp)) != EOF)
-	{
-		getyx(stdscr, y, x);
-		if (y == (row-1))
-		{
-			attron(A_REVERSE);
-			printw("< - Press Any Key - >");
-			attroff(A_REVERSE);
-			getch();
-			clear();
-			move(0, 0);
-		}
-		printw("%c", ch);
-	}
-	printw("<END>");
-	getch();
-	endwin();
-	fclose(fp);
+	
 	return 0;
+}
+
+WINDOW *create_newwin(int height, int width, int startx, int starty)
+{
+	WINDOW *local_win;
+	local_win = newwin(height, width, startx, starty);
+	box(local_win, 0, 0);
+	wrefresh(local_win);
+	return local_win;
+}
+void destory_win(WINDOW *local_win)
+{
+	wborder(local_win, )
 }
